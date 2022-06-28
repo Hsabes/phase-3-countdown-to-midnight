5a8a8c62731def34747be40ccbdc23edc6352787
Harrison Sabean <hsabes@gmail.com>
2022-06-28 09:28:52 -0700

    commit tree 2e67729a6ce8eee087e125d95afbb3efce79d7c4

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,28 @@
 #write your code here
 
-def countdown
+def countdown(int)
+    message = ""
+    while (int > 0)
+        puts "#{int} SECOND(S)!"
+        int -= 1
+        if (int == 0)
+            message = "HAPPY NEW YEAR!"
+        end
+    end
+    message
 end
+
+def countdown_with_sleep(int)
+    message = ""
+    while (int > 0)
+        sleep 1 # second
+        puts "#{int} SECOND(S)!"
+        int -= 1
+        if (int == 0)
+            message = "HAPPY NEW YEAR!"
+        end
+    end
+    message
+end
+
+countdown_with_sleep(10)
